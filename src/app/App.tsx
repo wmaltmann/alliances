1fb036525b24a0e6fa38ed7513d3af8a9995668f
Mike Altmann <6716819+wmaltmann@users.noreply.github.com>
2024-05-10 14:44:05 -0500

    setup

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from '../routes/LoginPage';
+import HomePage from '../routes/HomePage2';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginPage/>} />
+            <Route path="*" element={<HomePage/>} />
+          </Routes>
+        </Router>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/HomePage2.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      {/* Add your home page content here */}
+    </div>
+  );
+};
+
+export default HomePage;
