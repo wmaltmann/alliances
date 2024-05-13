53a3383c3aa4523f233c452a42ebf70e5de0fd25
Mike Altmann <6716819+wmaltmann@users.noreply.github.com>
2024-05-13 14:36:48 -0500

    eslint fixed

--- src/app/service-worker.ts
+++ src/app/service-worker.ts
@@ -10,7 +10,7 @@
 
 import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
-import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { createHandlerBoundToURL, precacheAndRoute } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
 
@@ -73,7 +73,7 @@ registerRoute(
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener("message", (event) => {
 	if (event.data && event.data.type === "SKIP_WAITING") {
-		self.skipWaiting();
+		void self.skipWaiting();
 	}
 });
 
