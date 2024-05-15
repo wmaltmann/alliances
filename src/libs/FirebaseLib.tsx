6acc7dbce7613e4b78cf522761d3fd1fdc7e78fd
Mike Altmann <6716819+wmaltmann@users.noreply.github.com>
2024-05-14 21:19:57 -0500

    setup dev deployment

--- src/libs/FirebaseLib.tsx
+++ src/libs/FirebaseLib.tsx
@@ -2,7 +2,21 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
+export const getAppStage = () => {
+	return process.env.REACT_APP_STAGE === "prod" ? "prod" : "dev";
+};
+
+const DEV_CONFIG = {
+	apiKey: "AIzaSyADZYyWAe6YydGBcrTfoTgJNyoJoLBsVh4",
+	authDomain: "alliance-selector-beta.firebaseapp.com",
+	databaseURL: "https://alliance-selector-beta-default-rtdb.firebaseio.com",
+	projectId: "alliance-selector-beta",
+	storageBucket: "alliance-selector-beta.appspot.com",
+	messagingSenderId: "397781631571",
+	appId: "1:397781631571:web:76cea58abcc50dec91bca6",
+};
+
+const PROD_CONFIG = {
 	apiKey: "AIzaSyDhHO7NY6zq87AG0QHdcDmJss8hNmZjSfo",
 	authDomain: "alliance-selector-d9518.firebaseapp.com",
 	databaseURL: "https://alliance-selector-d9518-default-rtdb.firebaseio.com",
@@ -12,6 +26,6 @@ const firebaseConfig = {
 	appId: "1:255656016827:web:0fdb8d06d491667c87a29e",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(getAppStage() === "dev" ? DEV_CONFIG : PROD_CONFIG);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
