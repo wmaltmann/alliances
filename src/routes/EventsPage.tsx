166935b8e822fa94b62d0cc0d6c84ff3190b19e1
Mike Altmann <6716819+wmaltmann@users.noreply.github.com>
2024-10-10 22:24:49 -0500

    Update EventsPage.tsx

--- src/routes/EventsPage.tsx
+++ src/routes/EventsPage.tsx
@@ -1,4 +1,5 @@
-import { Stack } from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
+import { Stack, Typography } from "@mui/material";
 import { FC } from "react";
 import EventList from "../components/common/EventList";
 import Page from "../components/page/Page";
@@ -8,10 +9,28 @@ const EventsPage: FC = () => {
 	const events: Event[] = [
 		{ id: "1", name: "Event 1" },
 		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
+		{ id: "2", name: "Event 2" },
 	];
 	return (
 		<Page>
 			<Stack height="100%" width="100%">
+				<Stack direction="row">
+					<Typography variant="h2">Alliances</Typography>
+					<AccountCircle />
+				</Stack>
 				<EventList events={events}></EventList>
 			</Stack>
 		</Page>
